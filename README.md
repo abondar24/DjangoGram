7756ea9d626b35cefcc5aa87d9252d31bd778c81
Alex Bondar <abondar1992@gmail.com>
2024-09-16 20:37:35 +0200

    added user profile

---
+++ README.md
@@ -0,0 +1,12 @@
+#DjangoGram
+
+
+## RUN
+
+python manage.py createsuperuser
+
+python manage.py migrate
+
+python manage.py makemigrations
+
+python manage.py runserver
--- djangogram/core/models.py
+++ djangogram/core/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model()
+
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField
+    bio = models.TextField(blank=True)
+    profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.CharField(max_length=100, blank=100)
+
+
+def __str__(self):
+    return self.user.username
