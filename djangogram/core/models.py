26eb986509000383b672a309e64d1005254eedb4
Alex Bondar <abondar1992@gmail.com>
2024-09-16 21:47:25 +0200

    profile sign up

--- djangogram/core/models.py
+++ djangogram/core/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 # Create your models here.
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_user = models.IntegerField
+    id_user = models.IntegerField(default=0)
     bio = models.TextField(blank=True)
     profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     location = models.CharField(max_length=100, blank=100)
--- djangogram/core/views.py
+++ djangogram/core/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+from .models import Profile
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email)
+                user.save()
+
+                ##TODO: login user and redirect to settings page
+
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model,id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('signup')
+
+    else:
+        return render(request, 'signup.html')
