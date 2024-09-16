76009c02f2dc70d298cf06179acdc160a525ef88
Alex Bondar <abondar1992@gmail.com>
2024-09-16 23:56:49 +0200

    added settings

--- djangogram/core/views.py
+++ djangogram/core/views.py
@@ -1,8 +1,7 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, auth
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-
 
 from .models import Profile
 
@@ -11,6 +10,35 @@ from .models import Profile
 @login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        if request.FILES.get('image') is None:
+            image = user_profile.profile_img
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profile_img = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        if request.FILES.get('image') is not None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profile_img = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')
+
+    return render(request,"settings.html", {'user_profile': user_profile})
 
 
 def signup(request):
@@ -31,12 +59,13 @@ def signup(request):
                 user = User.objects.create_user(username=username, password=password, email=email)
                 user.save()
 
-                ##TODO: login user and redirect to settings page
+                user_login = auth.authenticate(username=username,password=password)
+                auth.login(request, user_login)
 
                 user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model,id_user=user_model.id)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
 
         else:
             messages.info(request, 'Passwords do not match')
@@ -47,23 +76,23 @@ def signup(request):
 
 
 def signin(request):
-
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = auth.authenticate(username=username,password=password)
+        user = auth.authenticate(username=username, password=password)
 
         if user is not None:
             auth.login(request, user)
             return redirect('/')
         else:
-            messages.info(request,'Invalid credentials')
+            messages.info(request, 'Invalid credentials')
             return redirect('signin')
     else:
-        return render(request,'signin.html')
+        return render(request, 'signin.html')
 
+
+##FIX COOKIE SETTING ON LOGOUT
 @login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
-    request.session.flush()
     return redirect('signin')
